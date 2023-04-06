505d2c3312e67567c755f5e834e9c96a136fecd0
CarlZh <811095332@qq.com>
2023-04-05 20:47:39 -0400

    tailwind init

--- google-clone-nextjs-v13/src/app/page.jsx
+++ google-clone-nextjs-v13/src/app/page.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <><h1 className="text-3xl font-bold underline font-mono">
+      Hello world!
+    </h1><p class="font-sans text-red">The quick brown fox ...</p>
+    <p class="font-serif ...">The quick brown fox ...</p>
+    <p class="font-mono ...">The quick brown fox ...</p>
+    </>
   )
 }
