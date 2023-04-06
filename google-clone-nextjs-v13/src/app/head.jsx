37ea552a482cf05706c9484e7d787d787083e20b
CarlZh <811095332@qq.com>
2023-04-05 20:17:33 -0400

    1

---
+++ google-clone-nextjs-v13/src/app/head.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <>
+    <title>Google clone nextjs 13</title>
+    <meta content='width=device-width,initial-scale=1' name='viewport' />
+    <meta name='description' content='GoogleClone' />
+    </>
+  )
+}
+
+export default Head
