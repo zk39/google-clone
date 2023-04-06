203873556417a7da6c2ac7185588d741f8b8892b
CarlZh <811095332@qq.com>
2023-04-05 20:49:07 -0400

    css init

--- google-clone-nextjs-v13/src/app/head.jsx
+++ google-clone-nextjs-v13/src/app/head.jsx
@@ -6,6 +6,7 @@ function Head() {
     <title>Google clone nextjs 13</title>
     <meta content='width=device-width,initial-scale=1' name='viewport' />
     <meta name='description' content='GoogleClone' />
+    <link rel="icon" href="/1534129544.svg" />
     </>
   )
 }
