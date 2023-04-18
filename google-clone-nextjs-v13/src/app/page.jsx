c7162180674b54f872e62f4c533f0baa21e91d55
CarlZh <811095332@qq.com>
2023-04-17 20:50:13 -0400

    add header

--- google-clone-nextjs-v13/src/app/page.jsx
+++ google-clone-nextjs-v13/src/app/page.jsx
@@ -1,12 +1,10 @@
+import HomeHeader from '@/components/HomeHeader \u001A'
 import React from 'react'
 
 export default function Home() {
   return (
-    <><h1 className="text-3xl font-bold underline font-mono">
-      Hello world!
-    </h1><p class="font-sans text-red">The quick brown fox ...</p>
-    <p class="font-serif ...">The quick brown fox ...</p>
-    <p class="font-mono ...">The quick brown fox ...</p>
+    <>
+    <HomeHeader />
     </>
   )
 }
