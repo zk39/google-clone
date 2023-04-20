66374babbd7d3cb37760eac9f4427581475e9e71
CarlZh <811095332@qq.com>
2023-04-19 23:39:21 -0400

    fetch data

--- google-clone-nextjs-v13/src/app/search/web/page.jsx
+++ google-clone-nextjs-v13/src/app/search/web/page.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
-export default function WebSearchPage() {
-  return (
-    <div>WebSearchPage</div>
-  )
+export default async function WebSearchPage({searchParams}) {
+  const res = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_key}&cx=${process.env.CONTEXT_key}&q=${searchParams.searchTerm}`)
+  const data = await res.json();
+  const temp = data.items;
+  console.log(temp)
+    return <>{temp.map((res)=> <h1 key={data.kind}>  {res.title}</h1>)}
+    </>
 }
