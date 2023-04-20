00526b41015bc2f3c32cfbe197ef65c7ad95e302
CarlZh <811095332@qq.com>
2023-04-19 23:39:16 -0400

    fetch data

--- google-clone-nextjs-v13/src/app/page.jsx
+++ google-clone-nextjs-v13/src/app/page.jsx
@@ -12,6 +12,7 @@ export default function Home() {
           width="300"
           height="100"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+          alt="google svg"
         />
         <HomeSearch />
       </div>
